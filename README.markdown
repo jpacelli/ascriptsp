112335027e995f31a5fa0669b62371348aa87853
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-05-08 23:30:21 -0400

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+#NBDN - AllScripts - May 2011
+
+Prep work for the course!!
---
+++ start_new_exercise.ps1
@@ -0,0 +1,15 @@
+$now = get-date
+$new_branch_name = "$($now.year)$($now.month.tostring("00"))$($now.day.tostring("00"))$($now.hour.tostring("00"))$($now.minute.tostring("00"))"
+git add -A
+git commit -m "Committing"
+git checkout master
+
+$status = git status
+
+if ($status[0].endswith("master"))
+{
+  git checkout -b $new_branch_name
+  git pull jp master
+  echo "new branch name:$new_branch_name"
+}
+
